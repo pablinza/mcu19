f94d837cd5a0fc1b9d5eb75d77340a6925d4c241
pablo <pablo@tecra.bemicro>
2019-03-10 12:13:37 -0400

    Actualizacion 1903

---
+++ p877micro.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=p877micro.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/p877micro.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=p877micro.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/p877micro.x.tar
