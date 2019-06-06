03f912735f29e261b1d7ad6d3efbf84562278602
Pablo <pablinza@me.com>
2019-06-06 20:12:12 +0000

    Update 1906

---
+++ p887micro.X/dht.c
@@ -0,0 +1,88 @@
+/****************************************************************
+ *  Libreria DHT para los sensores DHT11/22 1-Wire				*
+ *  Autor: Pablo Zarate email:pablo@digital-bo.com				*
+ *  facebook group: ELECTRONICA MCU Version:1906				*
+ *  Material de uso para aprendizaje en ELT436 U.E.B.			*
+ * 	Requiere libreria periphera.h								*
+ * #define DHTTYPE <modelo> //11 = DHT11 o 22 = DHT22			*
+ * #define DHTpin PORTXbits.RXX //Pin para los datos I/O		*
+ * #define DHTtris TRISXbits.TRISXX //Control I/O				*
+ ****************************************************************/
+void DHTSetup()
+{
+    DHTtris = 1;  //Input mode for ZImp. pull-ups
+    DHTpin = 1;   //Logic 0 set pin to High on Input mode
+}
+char DHTReadbyte()
+{
+    char bitn = 8, res = 0, cnt;
+	DHTtris = 1; // TRIS is input(1)
+	while(bitn)
+    {
+		cnt = 0;
+        while(!DHTpin) //Next: wait until pin goes high max 55uS
+        {
+            //__delay_us(1);
+            //if(cnt++ > 50) //min 40uS max55uS, Estimated DHT22
+            //    return 0;
+        }
+        __delay_us(25); //26uS=0 logic 70uS=1 logic, Estimated DHT22
+        if(DHTpin)
+            res |= 0x01;
+		bitn --;
+        if(bitn) res <<= 1;
+        while (DHTpin); //Wait for end
+	}
+    return res;
+}
+
+char DHTUpdate(dhtstruct *dht)
+{
+    char cnt, chk, urh, lrh, ute, lte;
+    DHTtris = 0;    //Output mode
+    DHTpin = 0;     //Line to 0 for Host Start
+    if(DHTTYPE == 22)
+        __delay_ms(4); //DHT22 require 1ms 
+    if(DHTTYPE == 11)
+        __delay_ms(18);  //DHT11 require 18ms
+    DHTpin = 1;     //Line to 1
+    __delay_us(20); //Release line for 20uS
+    DHTtris = 1;    //Input mode
+    cnt = 0;
+    while(DHTpin)   //Wait for sensor response max 40uS
+    {
+        __delay_us(1);
+        if(cnt++ > 40) //min 20uS max40uS, Estimated DHT22
+            return 0;
+    }
+    cnt = 0;
+    while(!DHTpin) //wait in pull-down state edge max 80uS
+    {
+        __delay_us(1);
+        if(cnt++ > 80)  //max 80uS, Estimated
+            return 0;
+    }
+    cnt = 0;
+    while(DHTpin) //wait in pull-up state max 80uS
+    {
+        __delay_us(1);
+        if(cnt++ > 80) //max 80uS, Estimated wait in up level
+            return 0;
+    }
+    //Read 5byte data <HighRH><LowRH><HIGHTE><LOWTE><CHK>
+    putchar('S');
+    urh = DHTReadbyte();
+    lrh = DHTReadbyte();
+    ute = DHTReadbyte();
+    lte = DHTReadbyte();
+    printf("s1:%2u-%2u-%2u-%2u\r\n", urh, lrh, ute, lte);
+    chk = DHTReadbyte(); //Checksum
+    cnt = urh + lrh + ute + lte;
+    if(chk != cnt)
+        return 0;
+    dht->rawhum = urh << 8;  
+    dht->rawhum |= lrh;
+    dht->rawtem = ute << 8;
+    dht->rawtem |= lte;
+    return 1;   
+}
---
+++ p887micro.X/dht.h
@@ -0,0 +1,27 @@
+/****************************************************************
+ *  Libreria DHT para los sensores DHT11/22 1-Wire				*
+ *  Autor: Pablo Zarate email:pablo@digital-bo.com				*
+ *  facebook group: ELECTRONICA MCU Version:1906				*
+ *  Material de uso para aprendizaje en ELT436 U.E.B.			*
+ * 	Requiere libreria periphera.h								*
+ * #define DHTTYPE <modelo> //11 = DHT11 o 22 = DHT22			*
+ * #define DHTpin PORTXbits.RXX //Pin para los datos I/O		*
+ * #define DHTtris TRISXbits.TRISXX //Control I/O				*
+ ****************************************************************/
+#define DHT11 1 //40 bits <8b:intHU><8b:decHU><8b:intTE><8b:decTE><8b:chk>
+#define DHT22 2 //40 bits <16:HU><16b:TE><8b:chk>
+#if !defined(DHTpin) && !defined(DHTtris)
+	#error "Lib DHT debe definir DHTpin/DHTtris"
+#endif
+#if !defined(DHTTYPE)
+	#error "Lib DHT modelo DHTTYPE no especificado"
+#endif
+typedef struct
+{
+    unsigned int rawhum;
+    unsigned int rawtem;
+} dhtstruct;
+void DHTSetup();
+char DHTReadbyte();
+char DHTUpdate(dhtstruct *dht);
+#include "dht.c"
